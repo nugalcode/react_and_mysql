43fcf497dabac41745703f7e14767e591fbd8648
Jed Nugal <nugaljed@gmail.com>
2022-05-18 15:37:01 -0700

    Added state variables for all inputs

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
 
     const [name, setName] = useState("");
+    const [age, setAge] = useState(0);
+    const [country, setCountry] = useState("");
+    const [position, setPosition] = useState("");
+    const [wage, setWage] = useState(0);
 
   return (
       <div className="App">
