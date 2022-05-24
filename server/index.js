e69b9a5e380996f3dada0560d6850e25e0d5433d
Jed Nugal <nugaljed@gmail.com>
2022-05-24 15:44:47 -0700

    Added an insert query into a local db variable
    query is in a POST request /create.

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+// database that holds our employee information
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: '',
+    database: 'employeesystem'
+})
+
+app.post('/create', (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+
+    db.query('INSERT INTO employees (name, age, country, position, wage) VALUES (?, ?, ?, ?, ?)',
+        [name, age, country, position, wage],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.send("Values inserted");
+            }
+        }
+    );
+
+})
+
+app.listen(3001, () => {
+    console.log("server is running on port 3001");
+})
--- src/App.js
+++ src/App.js
@@ -9,22 +9,22 @@ function App() {
     const [position, setPosition] = useState("");
     const [wage, setWage] = useState(0);
 
-  return (
-      <div className="App">
-          <div className="information">
-              <label>Name </label>
-              <input type="text"/>
-              <label>Age </label>
-              <input type="number" />
-              <label>Country </label>
-              <input type="text" />
-              <label>Position </label>
-              <input type="text" />
-              <label>Wage </label>
-              <input type="number" />
-              <button> Add Employee </button>
-          </div>
-      </div >
+    return (
+        <div className="App">
+            <div className="information">
+                <label>Name </label>
+                <input type="text" onChange={(e) => setName(e.target.value)}/>
+                <label>Age </label>
+                <input type="number" onChange={(e) => setAge(e.target.value)}/>
+                <label>Country </label>
+                <input type="text" onChange={(e) => setCountry(e.target.value)}/>
+                <label>Position </label>
+                <input type="text" onChange={(e) => setPosition(e.target.value)}/>
+                <label>Wage </label>
+                <input type="number" onChange={(e) => setWage(e.target.value)}/>
+                <button> Add Employee </button>
+            </div>
+        </div >
   );
 }
 
